3f7dd01cb247fe390cd25732444ad012fcd730ab
Tiara Fatehana A <43510118+tiarafatehana@users.noreply.github.com>
2018-09-25 16:55:08 +0700

    Add files via upload

--- formawal.php
+++ formawal.php
@@ -1,5 +1,21 @@
-<?php
+<form action="proses.php" method="POST">
+	<table>
+	<tr>
+		<td>Username :</td>
+		<td><input type="text" name="username" style="width: 120px;height: 50px"></td>
+	</tr>
+	<tr>
+		<td>Password : </td>
+		<td><input type="password" name="pass"></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td><input type="hidden" name="data" value="<?php echo "$data"; ?>"></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td><input type="submit" name="submit" value="submit" style="color: black; width: 100px;height: 40px;border-radius: 10px"></td>
+	</tr></table>
+</form>
 
 
-
-?>
--- proses.php
+++ proses.php
@@ -1,4 +1,49 @@
 <?php
+if (isset($_POST['submit'])) {
+		$user = $_POST['username'];
+		$pass = $_POST['pass'];
+		$data= $_POST['data'];
+		$data.=$user."*".$pass."*"."||";
 
-
+	}
 ?>
+<form action=" " method="POST" enctype="multipart/form-data">
+	<table>
+	<tr>
+		<td>Nama:</td>
+		<td><input type="text" name="nama" style="width: 120px;height: 50px"></td>
+	</tr>
+
+	<tr>
+		<td><h2>Hobi</td></h2>
+	</tr>
+	<tr>
+		<td>
+			<br><input type="checkbox" name="pilih[]">Berenang
+			<br><input type="checkbox" name="pilih[]">Dengerin Musik
+			<br><input type="checkbox" name="pilih[]">Planner
+			<br><input type="checkbox" name="pilih[]">Badminton
+			<br><input type="checkbox" name="pilih[]">Tidur
+			<br><input type="checkbox" name="pilih[]">Makan
+		</td>
+	</tr>
+	<tr>
+		<td>Gambar : </td>
+		<td><input type="file" name="foto"></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td><input type="submit" name="submit" value="submit" style="color: black; width: 100px;height: 40px;border-radius: 10px"></td>
+	</tr>
+</table>
+</form>
+
+<?php
+	if (isset($_POST['submit'])) {
+		$gambar=$_FILES['foto']['name'];
+		$hobi=$_POST['pilih'];
+
+		echo "<img src='$gambar'>";
+	}
+
+
